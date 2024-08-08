3a2359afeafa51e5db9c190c61e5724acb0157b3
zeus254 <ckarimi676@gmail.com>
2024-08-08 17:56:13 +0000

    First letter fix

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- A SQL script that creates an index idx_name_first on the table names and the first letter of name and score
+-- Index implimentation
+CREATE INDEX idx_name_first_score ON names (name(1), score);
